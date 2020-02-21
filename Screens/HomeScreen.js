c7c3156e7b5a1bcde0d9b1f32c51041cd32d6ea6
Mario Palmis <1522680@brunel.ac.uk>
2020-02-21 20:30:09 +0000

    Main Menu - Code Cleaning

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -46,28 +46,61 @@ const DATA = [
 
 function Item({ overlay, title, description }) {
     return (
-        <CardComponent 
-        overlayString={overlay}
-        titleString={title}
-        descriptionString={description}
+        <CardComponent
+            overlayString={overlay}
+            titleString={title}
+            descriptionString={description}
         />
     );
+}
+const MainMenu = () => {
+    return(
+        <FlatList
+        data={DATA}
+        renderItem={({ item }) =>
+            <Item
+                overlay={item.overlay}
+                title={item.title}
+                description={item.description}
+            />}
+        keyExtractor={item => item.id}
+    />
+    )  
 }
 
 class HomeScreen extends Component {
     render() {
         return (
-            <FlatList
-                data={DATA}
-                renderItem={({ item }) => 
-                <Item 
-                overlay={item.overlay}
-                title={item.title} 
-                description ={item.description}
-                />}
-                keyExtractor={item => item.id}
-            />
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.titleText}>döts</Text>
+                </View>
+                <View style={styles.body}>
+                    <MainMenu/>
+                </View>
+            </View>
         )
     }
 }
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        paddingLeft: 16,
+        maxHeight: 42,
+        flex: 1,
+    },
+    body: {
+        flex: 1,
+        flexGrow: 1,
+        alignSelf: 'stretch',
+    },
+    titleText: {
+        fontSize: 34,
+        fontFamily: 'Avenir-Heavy',
+        color: '#2C4159',
+    },
+});
