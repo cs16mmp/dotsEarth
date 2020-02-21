21435334284ff1c2cb0f17f17ac996957ff9e1b7
Mario Palmis <1522680@brunel.ac.uk>
2020-02-21 21:00:42 +0000

    Main Menu using JSON

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -9,39 +9,11 @@ import {
     ScrollView,
 } from 'react-native';
 import CardComponent from '../Components/CardComponent'
+import database from "../databases/data.json"
 
-const DATA = [
-    {
-        id: '01',
-        overlay: 'Burrell Street',
-        title: 'Clinics',
-        description: 'Book an appointment in seconds',
-    },
-    {
-        id: '02',
-        overlay: 'SECURE SEARCH',
-        title: 'Organisations',
-        description: 'Find LGBTQI+ organisations across the UK',
-    },
-    {
-        id: '04',
-        overlay: 'INFO',
-        title: 'STDs',
-        description: 'Find information about STDs and their symptoms',
-    },
-    {
-        id: '05',
-        overlay: 'NOTIFICATIONS',
-        title: 'Reminders',
-        description: 'Manage your friendly reminders',
-    },
-    {
-        id: '06',
-        overlay: 'Arrive in approximate 3 days',
-        title: 'Order Testing Kit',
-        description: 'Order a free and confidential home testing kit',
-    },
-];
+var data = JSON.parse(JSON.stringify(database));
+var DATA = []
+DATA = data.MainMenuItems;
 
 
 function Item({ overlay, title, description }) {
@@ -54,18 +26,18 @@ function Item({ overlay, title, description }) {
     );
 }
 const MainMenu = () => {
-    return(
+    return (
         <FlatList
-        data={DATA}
-        renderItem={({ item }) =>
-            <Item
-                overlay={item.overlay}
-                title={item.title}
-                description={item.description}
-            />}
-        keyExtractor={item => item.id}
-    />
-    )  
+            data={DATA}
+            renderItem={({ item }) =>
+                <Item
+                    overlay={item.overlay}
+                    title={item.title}
+                    description={item.description}
+                />}
+            keyExtractor={item => item.id}
+        />
+    )
 }
 
 class HomeScreen extends Component {
@@ -76,7 +48,7 @@ class HomeScreen extends Component {
                     <Text style={styles.titleText}>döts</Text>
                 </View>
                 <View style={styles.body}>
-                    <MainMenu/>
+                    <MainMenu />
                 </View>
             </View>
         )
